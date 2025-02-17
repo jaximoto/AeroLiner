9aee12ec4232220b4b5bcf16a68adf8a937086e3
jaximoto <62913915+jaximoto@users.noreply.github.com>
2025-02-16 19:40:38 -0800

    Can now only draw lines from planes

--- Assets/Scripts/Lines/LineGenerator.cs
+++ Assets/Scripts/Lines/LineGenerator.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class LineGenerator : MonoBehaviour
 {
     public GameObject linePrefab;
+    public LayerMask layerMask;
 
+    // private vars
     Line activeLine;
+    RaycastHit2D hit;
+    
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,10 +20,21 @@ public class LineGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // New Line needs to start at gameobject that is a plane:
+
+        // get mouse position
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       
+        // Check if clicking left mouse button and on a plane
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject newLine = Instantiate(linePrefab);
-            activeLine = newLine.GetComponent<Line>();
+            hit = Physics2D.Raycast(mousePos, Vector2.zero, layerMask);
+            if (hit.collider != null)
+            {
+                GameObject newLine = Instantiate(linePrefab);
+                activeLine = newLine.GetComponent<Line>();
+            }
+           
         }
 
         if (Input.GetMouseButtonUp(0)) 
@@ -27,7 +44,7 @@ public class LineGenerator : MonoBehaviour
 
         if (activeLine != null)
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
             activeLine.UpdateLine(mousePos);
         }
     }
