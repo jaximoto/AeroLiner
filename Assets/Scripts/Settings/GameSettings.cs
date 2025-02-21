27780a4f9bbd233d6ffabd80a3726c063a1849ac
jaximoto <62913915+jaximoto@users.noreply.github.com>
2025-02-20 19:54:19 -0800

    Added end game canvas

--- Assets/Scripts/Settings/GameSettings.cs
+++ Assets/Scripts/Settings/GameSettings.cs
@@ -1,9 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class GameSettings : MonoBehaviour
 {
-   // Airports
-   public float AngleThreshold;
+    // Airports
+    public float AngleThreshold;
     public float planeRotationSpeed;
+
+    // End Game Settings
+    public float GameOverUIFadeInDuration;
+
+    public bool gameEnded = false;
+    public bool endingGame = false;
+    public CanvasGroup endGameUI;
+
+
+    private void Update()
+    {
+        if (gameEnded) 
+            EndGame();
+    }
+
+    public void EndGame()
+    {
+        if (endingGame)
+            return;
+
+        endingGame = true;
+
+        FadeIn();
+
+    }
+
+    public void FadeIn()
+    {
+        // Fade in the CanvasGroup
+        LeanTween.alphaCanvas(endGameUI, 1f, GameOverUIFadeInDuration).setOnComplete(() =>
+        {
+            endGameUI.interactable = true;
+            endGameUI.blocksRaycasts = true;
+        });
+    }
 }
